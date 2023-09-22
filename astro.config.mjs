415a6c6cb2f1871265f4266d1f51e4ef3db46226
Jesús Mira <jesmira@hotmail.es>
2023-09-22 10:31:13 +0200

    last change

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jesusmira.github.io',
-  base: '/frontendmentor-flio-dar',
+  base: '/frontendmentor-flio-dark',
   integrations: [tailwind(), react()],
 });
